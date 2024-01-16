d603993bd546fb662343d00ae21966b4ffafd033
mauro.malafronte <mauriade.mal@gmail.com>
2024-01-16 15:29:05 +0100

    Aggiungere i file di progetto.

---
+++ BlazorChat2/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChat2.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task SendMessage(string userName, string message)
+        {
+            //invia il messaggio a tutti i client nella Hub
+            await Clients.All.SendAsync("ReceiveMessage",userName, message);
+        }
+    }
+}
---
+++ BlazorChat2/Model/UserMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorChat2.Model
+{
+    public class UserMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public bool IsCurrentUser { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
